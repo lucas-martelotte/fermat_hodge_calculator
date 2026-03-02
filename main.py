294b7aaec3a028a190313ad659f6a36092cd2802
Lucas <lucas.martelotte@gmail.com>
2026-03-02 10:55:20 -0300

    some tests

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# from sage.libs.pari import pari
-# pari.allocatemem(4_000_000_000)
+from sage.libs.pari import pari
+
+pari.allocatemem(4_000_000_000)
+
 from src.diagonal_variety import EvenDimensionalDiagonalVariety
 from src.formal_algebra import (
     FormalCyclotomicField,
     PolynomialRingOverFormalNumberField,
 )
-
+from src.utils.sage_imports import identity_matrix
+from src.utils.auxiliary import sage_matrix_map
 from src.hodge_calculator import HodgeCalculator, BasicHodgeCalculator
 from src.hodge_cycles.hodge_cycle_factories import LineFactory
 from src.hodge_calculator import HodgeCalculatorFactory
@@ -13,26 +16,42 @@ from src.hodge_calculator import BasicHodgeCalculator
 from time import perf_counter
 import json
 
-"""
-d = 3
+
+start_time = perf_counter()
+
+# degree 8: 1655 seconds (standard methods)
+# degree 8: rational methods (11 seconds)
+d = 12
 hodge_calculator_factory = HodgeCalculatorFactory()
 X, calculator = hodge_calculator_factory.create((d, d, d, d))
-hodge_period_matrix = calculator.get_period_matrix_of_primitive_hodge_cycles()
-line_data = calculator.get_hodge_cycle_factory_data("lines")
-line_coords = line_data["coordinates"]
-line_periods = line_data["period_matrix"]
+K_formal = X.formal_base_field
+print("Importing period matrix.")
+P = calculator.get_period_matrix_of_primitive_hodge_cycles()
+print("Size of period matrix", P.nrows(), P.ncols())
 
-print(line_periods)
-print()
-print(hodge_period_matrix.nrows(), hodge_period_matrix.ncols())
-print(line_coords.nrows(), line_coords.ncols())
-print((hodge_period_matrix.T * line_coords).T)
+Prat = K_formal.convert_to_rational_horizontally_blocked_matrix(P)
+print("Size of blocked period matrix", Prat.nrows(), Prat.ncols())
+print("Computing right-inverse.")
+Q = Prat.pseudoinverse()
+print("Right-inverse computed. Size: ", Q.nrows(), Q.ncols())
 
-print()
-print("======================================")
-print()
-"""
 
+end_time = perf_counter()
+print(end_time - start_time)
+
+exit()
+
+print("Computing right-inverse.")
+Q = P.pseudoinverse()
+print("Right-inverse computed. Size: ", Q.nrows(), Q.ncols())
+print("Exporting...")
+data = {"right_inverse": sage_matrix_map(str, Q)}
+with open(f"./right_inverse_deg_{d}.json", "w") as f:
+    json.dump(data, f)
+print("Exported successfully!")
+
+
+exit()
 
 for d in [5]:
 
